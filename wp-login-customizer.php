4c6813c86d2f4c32b6c8ef916788ad46faac998a
vivek <vivek_msn@hotmail.com>
2024-12-04 14:57:05 +0530

    add hooks and callback functions to create submenu

--- wp-login-customizer.php
+++ wp-login-customizer.php
@@ -11,3 +11,14 @@
  if(!defined("ABSPATH")){
     exit;
  }
+
+ add_action( "admin_menu", "wlc_add_submenu");
+
+ function wlc_add_submenu(){
+    add_submenu_page( "options-general.php", "WP Login Page Customizer", "WP Login Page Customizer", "manage_options", "wp-login-page-customizer", "wlc_handle_login_settings_layout");
+ }
+
+//  Create Login Page Customizer Layout
+function wlc_handle_login_settings_layout(){
+    
+}
