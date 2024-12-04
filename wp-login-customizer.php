ce688d4c76ea9c6543811b181f4d99423db5f02f
vivek <vivek_msn@hotmail.com>
2024-12-04 13:29:06 +0530

    write plugin header comment

---
+++ wp-login-customizer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WP Login Page Customizer
+ * Description: This Plugin will customized Logo, Text Color, and Background Color
+ * Author:Vivek Saini
+ * Plugin URI:https://example.com/wp-login-page-customizer
+ * Author URI:https://ithinkservices.com
+ * Version: 1.0
+ */
+
+ if(!defined("ABSPATH")){
+    exit;
+ }
